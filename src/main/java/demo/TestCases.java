8a3633479ee795e537a60b0d63184f52c28f2d1a
Tahseen yousuf <tehseenyousuf@gmail.com>
2024-01-10 15:58:23 +0530

    ME_QA_XLEETCODE1_MODULE_L1

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,7 +1,10 @@
 package demo;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -39,7 +42,7 @@ public class TestCases {
 
     public void endTest()
     {
-        System.out.println("End Test: TestCases");
+        System.out.println("End Test: LeetCode");
         driver.close();
         driver.quit();
 
@@ -48,8 +51,88 @@ public class TestCases {
     
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
+        driver.get("https://leetcode.com/ ");
+        if(driver.getCurrentUrl().contains("leetcode")){
+            System.out.println("TestCase01 : PASSED");
+        }
+        else {
+            System.out.println("TestCase01 : FAILED");
+        }
+        System.out.println("end Test case: testCase01");
+    }
+
+    public  void testCase02(){
+        System.out.println("Start Test case: testCase02");
+        try {
+            driver.findElement(By.xpath("//span[text()='Sign in']")).click();
+            Thread.sleep(5000);
+            WebElement user =  driver.findElement(By.id("id_login"));
+            user.click();
+            user.sendKeys("Tahseen123");
+            WebElement pswd =  driver.findElement(By.id("id_password"));
+            pswd.click();
+            pswd.sendKeys("Bismillah@123");
+            driver.findElement(By.xpath("//button[@id='signin_btn']")).click();
+            Thread.sleep(5000);
+//            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.findElement(By.xpath("//a[@href='/problemset/all/']")).click();
+            Thread.sleep(5000);
+//            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            if(driver.getCurrentUrl().contains("problemset")){
+                System.out.println("url contains problemset");
+            }
+            else {
+                System.out.println("url does not contains problemset");
+            }
+            List<WebElement> questions = driver.findElements(By.xpath("//a[@class='h-5 hover:text-blue-s dark:hover:text-dark-blue-s']"));
+            for (int i = 1; i<= 5; i++) {
+                System.out.println(questions.get(i).getText());
+            }
+            System.out.println("TestCase02 : PASSED");
+
+        }
+        catch(Exception e) {
+            System.out.println("Something somewhere went wrong");
+        }
+
         System.out.println("end Test case: testCase02");
+    }
+
+    public  void testCase03(){
+        System.out.println("Start Test case: testCase03");
+        try {
+            driver.findElement(By.xpath("//a[@href='/problems/two-sum']")).click();
+            Thread.sleep(5000);
+            driver.getCurrentUrl().contains("two-sum");
+            System.out.println("TestCase03 : PASSED");
+
+        }
+        catch(Exception e) {
+            System.out.println("Something somewhere went wrong");
+        }
+
+        System.out.println("end Test case: testCase03");
+    }
+
+    public  void testCase04(){
+        System.out.println("Start Test case: testCase04");
+        try {
+            driver.findElement(By.xpath("//span[@id='navbar_user_avatar']")).click();
+            driver.findElement(By.xpath("//div[text()='Sign Out']")).click();
+            Thread.sleep(15000);
+            driver.findElement(By.id("submissions_tab")).click();
+            Thread.sleep(4000);
+            WebElement result = driver.findElement(By.xpath("//a[@href='/accounts/login/?next=%2Fproblems%2Ftwo-sum%2Fsubmissions%2F']"));
+            if(result.getText().equals("Register or Sign In")) {
+                System.out.println("TestCase04 : PASSED");
+            }
+
+        }
+        catch(Exception e) {
+            System.out.println("Something somewhere went wrong");
+        }
+
+        System.out.println("end Test case: testCase04");
     }
 
 
